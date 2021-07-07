ff722ea4c86d6ea940266d67a7f61329b6dbb3a0
ROMSG <48342780+ROMSG@users.noreply.github.com>
2021-07-07 09:11:29 -0700

    Update config.fs

--- config.fs
+++ config.fs
@@ -115,3 +115,14 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
+[sns/]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[mpt/]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
